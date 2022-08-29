b4a88832bd4a2449973d00fb88c84dad4e89a8d2
Harshad Ranganathan <32612787+HarshadRanganathan@users.noreply.github.com>
2022-08-29 23:13:26 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,4 +1,25 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.20.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.1"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.1"
+    }
+  }
 }
